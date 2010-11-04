ec26d00c4b5a710e767816663fc6da3bdf8b578f
Afzal Najam <@>
2010-11-04 03:33:11 +0000

    Added MPL 1.1 block and LICENSE.txt. Also added AgencyAction.java which I had forgotten in the last commit

---
+++ android-gtfs-reader/src/afzal/gtfsReader/AgencyAction.java
@@ -0,0 +1,65 @@
+/****** BEGIN LICENSE BLOCK *****
+	Version: MPL 1.1/GPL 2.0/LGPL 2.1
+	
+	The contents of this file are subject to the Mozilla Public License Version 
+	1.1 (the "License"); you may not use this file except in compliance with 
+	the License. You may obtain a copy of the License at 
+	http://www.mozilla.org/MPL/
+	
+	Software distributed under the License is distributed on an "AS IS" basis,
+	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	for the specific language governing rights and limitations under the
+	License.
+	
+	The Original Code is located at http://code.google.com/p/android-gtfs-reader/
+	
+	The Initial Developer of the Original Code is
+	Afzal Najam <afzal.naj@gmail.com>
+	
+	Portions created by the Initial Developer are Copyright (C) 2010
+	Afzal Najam. All Rights Reserved.
+	
+	***** END LICENSE BLOCK ******/
+
+package afzal.gtfsReader;
+
+public class AgencyAction {
+	
+	private int label;
+	private String data;
+	private int type;
+	
+	public static final int ACTION_CALL = 1;
+	public static final int ACTION_VIEW = 2;
+	
+	public AgencyAction(int label, String data, int type) {
+		super();
+		this.label = label;
+		this.data = data;
+		this.type = type;
+	}
+	
+	public int getLabel(){
+		return label;
+	}
+	
+	public void setLabel(int label) {
+		this.label = label;
+	}
+	
+	public String getData() {
+		return data;
+	}
+	
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	public int getType() {
+		return type;
+	}
+	
+	public void setType(int type) {
+		this.type = type;
+	}
+}
--- android-gtfs-reader/src/afzal/gtfsReader/DBAdapter.java
+++ android-gtfs-reader/src/afzal/gtfsReader/DBAdapter.java
@@ -1,3 +1,26 @@
+/****** BEGIN LICENSE BLOCK *****
+	Version: MPL 1.1/GPL 2.0/LGPL 2.1
+	
+	The contents of this file are subject to the Mozilla Public License Version 
+	1.1 (the "License"); you may not use this file except in compliance with 
+	the License. You may obtain a copy of the License at 
+	http://www.mozilla.org/MPL/
+	
+	Software distributed under the License is distributed on an "AS IS" basis,
+	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+	for the specific language governing rights and limitations under the
+	License.
+	
+	The Original Code is located at http://code.google.com/p/android-gtfs-reader/
+	
+	The Initial Developer of the Original Code is
+	Afzal Najam <afzal.naj@gmail.com>
+	
+	Portions created by the Initial Developer are Copyright (C) 2010
+	Afzal Najam. All Rights Reserved.
+	
+	***** END LICENSE BLOCK ******/
+
 package afzal.gtfsReader;
 
 import android.content.ContentValues;
